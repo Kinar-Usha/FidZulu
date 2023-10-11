7f24b9e08cc934d371c2e23091d74168afa160f7
Associate <Associate@roifmrwindt>
2023-10-11 14:40:58 +0530

    github actions error resolved

--- bikeService/dao/daoImpl.js
+++ bikeService/dao/daoImpl.js
@@ -1,52 +1,50 @@
 const AWS = require('aws-sdk');
 const dotenv = require('dotenv');
 const path= require('path');    
+const fs= require('fs');
 const rootPath= path.resolve(__dirname, '../');
 dotenv.config({ path: path.resolve(rootPath, './.env') })
+const jsonRootPath= path.resolve(__dirname, '../../');
+const filepath=path.join(jsonRootPath, 'resources/Bikejson.json');
 
+var circuitBreakerFlag=false
+if (process.env.AWS_REGION && process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
 
+var docClient;
 // Configure your AWS credentials
 AWS.config.update({
     region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   });
-
-
-
-// Create a DynamoDB Document Client
-const docClient = new AWS.DynamoDB.DocumentClient();
-
+   docClient = new AWS.DynamoDB.DocumentClient();
+}else{
+    circuitBreakerFlag=true;
+    // Create a DynamoDB Document Client
 // DynamoDB table name
+}
+
+
 const tableName = 'fz_bikes';
 
-// Function to get all items from DynamoDB
-function getAllDataFromDynamoDB() {
 
-  const params = {
-    TableName: tableName,
-  };
 
-  docClient.scan(params, (err, data) => {
-    if (err) {
-      console.error('Error:', err);
-    } else {
-      if (data.Items) {
-        return data.Items;
-      } else {
-        console.log('No items found in the table.');
-      }
-    }
-  });
-}
+
+
+
 
 // Call the function to retrieve all items from the DynamoDB table
 function getAllDataFromDynamoDB(callback) {
+   
     const params = {
-        TableName: tableName,
+        TableName: tableName || 'fz_bikes',
     };
 
     return new Promise((resolve, reject) => {
+        if(circuitBreakerFlag){
+            resolve ( JSON.parse(fs.readFileSync(filepath)));
+    
+        }
         docClient.scan(params, (err, data) => {
           if (err) {
             console.error('Error:', err);
--- foodService/dao/daoImpl.js
+++ foodService/dao/daoImpl.js
@@ -1,51 +1,50 @@
 const AWS = require('aws-sdk');
 const dotenv = require('dotenv');
 const path= require('path');    
+const fs= require('fs');
 const rootPath= path.resolve(__dirname, '../');
 dotenv.config({ path: path.resolve(rootPath, './.env') })
+const jsonRootPath= path.resolve(__dirname, '../../');
+const filepath=path.join(jsonRootPath, 'resources/Foodjson.json');
 
+var circuitBreakerFlag=false
+if (process.env.AWS_REGION && process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
+var docClient;
+    
 // Configure your AWS credentials
 AWS.config.update({
     region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   });
+   docClient = new AWS.DynamoDB.DocumentClient();
 
-
-
-// Create a DynamoDB Document Client
-const docClient = new AWS.DynamoDB.DocumentClient();
-
+}else{
+    circuitBreakerFlag=true;
+    // Create a DynamoDB Document Client
 // DynamoDB table name
+}
+
 const tableName = 'fz_food';
 
-// Function to get all items from DynamoDB
-function getAllDataFromDynamoDB() {
 
-  const params = {
-    TableName: tableName,
-  };
 
-  docClient.scan(params, (err, data) => {
-    if (err) {
-      console.error('Error:', err);
-    } else {
-      if (data.Items) {
-        return data.Items;
-      } else {
-        console.log('No items found in the table.');
-      }
-    }
-  });
-}
+
+
+
 
 // Call the function to retrieve all items from the DynamoDB table
 function getAllDataFromDynamoDB(callback) {
+   
     const params = {
-        TableName: tableName,
+        TableName: tableName || 'fz_food',
     };
 
     return new Promise((resolve, reject) => {
+        if(circuitBreakerFlag){
+            resolve ( JSON.parse(fs.readFileSync(filepath)));
+    
+        }
         docClient.scan(params, (err, data) => {
           if (err) {
             console.error('Error:', err);
